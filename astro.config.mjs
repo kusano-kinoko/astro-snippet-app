0cbcf10bddc56e7f0d8c5d8e6bc95db718c47749
kusan <kusanomasamunekyoto@gmail.com>
2025-12-16 22:33:00 +0900

    added

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   site: "https://kusano-kinoko.github.io",
-  base: "/astro-snippet-app",
+  base: "/astro-snippet-app/",
 
   vite: {
     plugins: [tailwindcss()],
